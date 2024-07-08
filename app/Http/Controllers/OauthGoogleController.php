dd9399bb0797b56845dc4810860a600529a63798
Nerofann <crazyhx4@gmail.com>
2024-07-09 01:06:04 +0700

    Handle register with google

--- app/Http/Controllers/OauthGoogleController.php
+++ app/Http/Controllers/OauthGoogleController.php
@@ -6,7 +6,9 @@ use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
 
 class OauthGoogleController extends Controller
 {
@@ -25,25 +27,33 @@ class OauthGoogleController extends Controller
                 return redirect()->route( "dashboard" )->with("success", "Selamat datang kembali {$user->first_name}");
             }
 
-            dd($gUser);
             /** Jika Belum punya akun */
-            // $register = User::create([
-            //     'uuid'          => Str::uuid(),
-            //     'first_name'    => $request->get('first-name'),
-            //     'last_name'     => $request->get('last-name'),
-            //     'username'      => Str::lower($request->get('first-name') . "_" . $request->get('last-name')),
-            //     'email'         => $request->get('email'),
-            //     'password'      => Hash::make($request->get('password')),
-            //     'referral'      => Str::random(10),
-            //     'crated_at '    => date("Y-m-d H:i:s")
-            // ]);
+            $register = User::create([
+                'uuid'          => Str::uuid(),
+                'first_name'    => $gUser->user['given_name'],
+                'last_name'     => $gUser->user['family_name'],
+                'username'      => Str::snake(Str::lower($gUser->user['given_name'] . "_" . $gUser->user['family_name'])),
+                'email'         => $gUser->email,
+                'password'      => Hash::make($gUser->id),
+                'referral'      => Str::random(10),
+                'crated_at '    => date("Y-m-d H:i:s")
+            ]);
     
-            // if(!$register) {
-            //     return redirect()->route('register')->withInput($request->all())->withErrors($validate->errors())->with('email', "System mainctance");
-            // }
+            if(!$register) {
+                $input  = [
+                    'first-name'    => $gUser->user['given_name'],
+                    'last-name'     => $gUser->user['family_name'],
+                    'email'         => $gUser->email,
+                ];
+
+                return redirect()->route('register')->withInput($input)->with('danger', "Mohon registrasi manual");
+            }
+
+            return redirect()->route('login')->with('success', "Berhasil didaftarkan, mohon login");
+
 
         } catch (Exception $e) {
-            return redirect()->route('login')->with('danger', $e->getMessage());
+            return redirect()->route('login')->with('danger', "[EXCEPTION] Code: " . $e->getCode());
         }
     }
 }
